639216f4386ebd1b10137e7423b1e3eb0775bff1
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-04-08 00:37:17 +0900

    ちょい待て

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert("hello");
 
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
-
-  const contentLadyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
-      <p style={contentStyle}>お元気ですか？</p>
-      <p style={contentLadyStyle}>元気です</p>
+      <ColorfulMessage color="blue" message="お元気ですか?"></ColorfulMessage>
+      <ColorfulMessage color="pink" message="元気です" />
       <button onClick={onClickButton}>ボタン</button>
     </React.Fragment>
   );
